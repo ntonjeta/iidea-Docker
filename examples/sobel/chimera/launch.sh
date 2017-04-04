27a624bfc29f3e811d35c249f7df00f306c9bce5
ntonjeta <ntonjeta@autistici.org>
2017-04-04 11:25:46 +0200

    some example path modified

--- examples/sobel/chimera/launch.sh
+++ examples/sobel/chimera/launch.sh
@@ -2,4 +2,4 @@
 set -x
 
 #rm -rf output
-../../../clang-chimera/build/clang-chimera -v -fun-op conf.csv -generate-mutants  ../src/sobel.cpp -o output -- -std=c++11  -I../src/boost_1_63_0 -I../src/include 
+clang-chimera -v -fun-op conf.csv -generate-mutants  ../src/sobel.cpp -o output -- -std=c++11  -I../src/boost_1_63_0 -I../src/include 
--- examples/sobel/src/sobel.cpp
+++ examples/sobel/src/sobel.cpp
@@ -10,96 +10,96 @@
 #include <cmath>
 
 
-char src_image_path[] = "/opt/Approximate/examples/sobel/image/original.rgb";
-char noaprx_image_path[] = "/opt/Approximate/examples/sobel/image/noaprx.rgb";
-char dst_image_path[] = "/opt/Approximate/examples/sobel/image/dst.rgb";
+//char src_image_path[] = "/opt/Approximate/examples/sobel/image/original.rgb";
+//char noaprx_image_path[] = "/opt/Approximate/examples/sobel/image/noaprx.rgb";
+//char dst_image_path[] = "/opt/Approximate/examples/sobel/image/dst.rgb";
 
-double eDistance(boost::shared_ptr<Pixel> p, boost::shared_ptr<Pixel> q) {
-  double r = 0;
+//double eDistance(boost::shared_ptr<Pixel> p, boost::shared_ptr<Pixel> q) {
+  //double r = 0;
 
-  double r_tmp;
+  //double r_tmp;
   
-  r += (p->r - q->r) * (p->r - q->r);
-  r += (p->g - q->g) * (p->g - q->g);
-  r += (p->b - q->b) * (p->b - q->b);
+  //r += (p->r - q->r) * (p->r - q->r);
+  //r += (p->g - q->g) * (p->g - q->g);
+  //r += (p->b - q->b) * (p->b - q->b);
 
-  r_tmp = sqrt(r);
+  //r_tmp = sqrt(r);
 
-  return r_tmp;
-}
+  //return r_tmp;
+//}
 
-void filter (boost::shared_ptr<Image> srcImagePtr,boost::shared_ptr<Image> dstImagePtr){
+//void filter (boost::shared_ptr<Image> srcImagePtr,boost::shared_ptr<Image> dstImagePtr){
 
-  int x, y;
-  float s = 0;
-  float w[][3] = {
-    {0, 0, 0},
-    {0, 0, 0},
-    {0, 0, 0}
-  };
+  //int x, y;
+  //float s = 0;
+  //float w[][3] = {
+    //{0, 0, 0},
+    //{0, 0, 0},
+    //{0, 0, 0}
+  //};
 
 
 
-  y = 0 ;
+  //y = 0 ;
     
-  // Start performing Sobel operation
-  for( x = 0 ; x < srcImagePtr->width ; x++ ) {
-    HALF_WINDOW(srcImagePtr, x, y, w) ;
+  //// Start performing Sobel operation
+  //for( x = 0 ; x < srcImagePtr->width ; x++ ) {
+    //HALF_WINDOW(srcImagePtr, x, y, w) ;
     
-    s = sobel(w);
+    //s = sobel(w);
 
-    dstImagePtr->pixels[y][x]->r = s ;
-    dstImagePtr->pixels[y][x]->g = s ;
-    dstImagePtr->pixels[y][x]->b = s ;
-  }
+    //dstImagePtr->pixels[y][x]->r = s ;
+    //dstImagePtr->pixels[y][x]->g = s ;
+    //dstImagePtr->pixels[y][x]->b = s ;
+  //}
 
-  for (y = 1 ; y < (srcImagePtr->height - 1) ; y++) {
-    x = 0 ;
-    HALF_WINDOW(srcImagePtr, x, y, w);
+  //for (y = 1 ; y < (srcImagePtr->height - 1) ; y++) {
+    //x = 0 ;
+    //HALF_WINDOW(srcImagePtr, x, y, w);
 
-    s = sobel(w);
+    //s = sobel(w);
       
-    dstImagePtr->pixels[y][x]->r = s ;
-    dstImagePtr->pixels[y][x]->g = s ;
-    dstImagePtr->pixels[y][x]->b = s ;
+    //dstImagePtr->pixels[y][x]->r = s ;
+    //dstImagePtr->pixels[y][x]->g = s ;
+    //dstImagePtr->pixels[y][x]->b = s ;
 
-    for( x = 1 ; x < srcImagePtr->width - 1 ; x++ ) {
-      WINDOW(srcImagePtr, x, y, w) ;
+    //for( x = 1 ; x < srcImagePtr->width - 1 ; x++ ) {
+      //WINDOW(srcImagePtr, x, y, w) ;
               
-      s = sobel(w);
+      //s = sobel(w);
 
-      dstImagePtr->pixels[y][x]->r = s ;
-      dstImagePtr->pixels[y][x]->g = s ;
-      dstImagePtr->pixels[y][x]->b = s ;
+      //dstImagePtr->pixels[y][x]->r = s ;
+      //dstImagePtr->pixels[y][x]->g = s ;
+      //dstImagePtr->pixels[y][x]->b = s ;
 
-    }
+    //}
 
-    x = srcImagePtr->width - 1 ;
-    HALF_WINDOW(srcImagePtr, x, y, w) ;
+    //x = srcImagePtr->width - 1 ;
+    //HALF_WINDOW(srcImagePtr, x, y, w) ;
               
 
-    s = sobel(w);
+    //s = sobel(w);
 
-    dstImagePtr->pixels[y][x]->r = s ;
-    dstImagePtr->pixels[y][x]->g = s ;
-    dstImagePtr->pixels[y][x]->b = s ;
-  }
+    //dstImagePtr->pixels[y][x]->r = s ;
+    //dstImagePtr->pixels[y][x]->g = s ;
+    //dstImagePtr->pixels[y][x]->b = s ;
+  //}
 
-  y = srcImagePtr->height - 1;
+  //y = srcImagePtr->height - 1;
 
-  for(x = 0 ; x < srcImagePtr->width ; x++) {
-    HALF_WINDOW(srcImagePtr, x, y, w) ;
+  //for(x = 0 ; x < srcImagePtr->width ; x++) {
+    //HALF_WINDOW(srcImagePtr, x, y, w) ;
             
-    s = sobel(w);
+    //s = sobel(w);
 
-    dstImagePtr->pixels[y][x]->r = s ;
-    dstImagePtr->pixels[y][x]->g = s ;
-    dstImagePtr->pixels[y][x]->b = s ;
+    //dstImagePtr->pixels[y][x]->r = s ;
+    //dstImagePtr->pixels[y][x]->g = s ;
+    //dstImagePtr->pixels[y][x]->b = s ;
 
-  }
+  //}
 
-  //dstImagePtr->saveRgbImage("med.rgb", std::sqrt(256 * 256 + 256 * 256)) ;
-}
+  ////dstImagePtr->saveRgbImage("med.rgb", std::sqrt(256 * 256 + 256 * 256)) ;
+//}
 
 int main ( int argc, const char* argv[])
 {
